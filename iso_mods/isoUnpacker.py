97ae5301e68cc33b80e1f18eb853a3b6dfc6621a
M Azwar Nurrosat <azwar.nrst@gmail.com>
2019-06-19 13:29:32 +0700

    Update isoUnpacker.py

--- iso_mods/isoUnpacker.py
+++ iso_mods/isoUnpacker.py
@@ -16,41 +16,16 @@ class isoUnpacker:
         self.dataElement = self.isomsg[20:]
 
     def unpack(self):
-        # self.activeBits =
-
-        # my_hexdata = "723A40010AE18002"
-        #
-        # scale = len(self.bitmap)
-        #
-        # num_of_bits = self.bitLen
-
         self.activeBits = bin(int(self.bitmap, len(self.bitmap)))[2:].zfill(self.bitLen)
-
         deMap = DataElements()
-
-        # self.processingCode = self.isodata[2:self.lenProcessingCode + 2]
-        # self.isodata = self.isodata[len(self.processingCode)+1:]
-        #
-        # self.amount = self.isodata[:self.lenAmount+1]
-        # self.isodata = self.isodata[len(self.lenAmount) + 1:]
-
         for bit in range(0, len(deMap.BITS_VALUE_TYPE)):
             print(deMap.getBitLabel(bit))
-
         print(self.activeBits)
         print("\n")
         cleanElement = {}
         for bit in range(0, len(self.activeBits)):
-            # print(bits[bit:bit+1])
             if self.activeBits[bit:bit + 1] == "1":
                 print("active bit", bit + 1, deMap.BITS_VALUE_TYPE[bit+1][1], deMap.BITS_VALUE_TYPE[bit+1][2], deMap.BITS_VALUE_TYPE[bit+1][3])
                 frmt = deMap.BITS_VALUE_TYPE[bit+1][2]
                 deLen = deMap.BITS_VALUE_TYPE[bit+1][3]
-
-                # cleanElement[bit+1] = self.isodata[deMap.getBitLen(bit+1):deMap.getBitLen(bit+1)+1]
-                # self.isodata = self.isodata[len(cleanElement[bit+1])+1:]
-
         return self
-
-
-
